89bd05387d24ec3d2797a08f00f7f18e9e33f471
Anders Waldenborg <anders@0x63.nu>
2013-01-18 13:53:29 +0100

    Add PartialWarningsCompile
    Works like Compile but adds three new arguments: partialwarningStart,
    partialwarningEnd, partialwarningProperty which makes warnings not to
    be collected until a line that matches the partialwarningStart regexp
    is found. Collection stops when partialwarningEnd matches. In addition
    to storing the collected warnings into a separate logfile the property
    given by partialwarningProperty is updated with the count.

---
+++ steps/partialwarnings.py
@@ -0,0 +1,108 @@
+# Copyright (C) 2008-2013 Anders Waldenborg et al.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from buildbot.steps.shell import Compile
+
+
+class PartialWarningsCompile(Compile):
+    partialwarningStart = None
+    partialwarningEnd = None
+    partialwarningProperty = None
+
+    def __init__(self, partialwarningStart=None, partialwarningEnd=None, partialwarningProperty=None, **kwargs):
+        Compile.__init__(self, **kwargs)
+        self.partialwarningStart = partialwarningStart
+        self.partialwarningEnd = partialwarningEnd
+        self.partialwarningProperty = partialwarningProperty
+        self.addFactoryArguments(partialwarningStart=self.partialwarningStart)
+        self.addFactoryArguments(partialwarningEnd=self.partialwarningEnd)
+        self.addFactoryArguments(partialwarningProperty=self.partialwarningProperty)
+
+    def describe(self, done=False):
+        ret = []
+        properties = self.build.getProperties()
+
+        if done and self.descriptionDone is not None:
+            for words in list(self.descriptionDone):
+                if isinstance(words, (str, unicode)):
+                    words = words.split()
+                words = properties.render(words)
+                if isinstance(words, list):
+                    ret.extend(words)
+                else:
+                    ret.append(words)
+            return ret
+        if self.description is not None:
+            for words in list(self.description):
+                if isinstance(words, (str, unicode)):
+                    words = words.split()
+                words = properties.render(words)
+                if isinstance(words, list):
+                    ret.extend(words)
+                else:
+                    ret.append(words)
+            return ret
+
+        words = self.command
+        if isinstance(words, (str, unicode)):
+            words = words.split()
+        # render() each word to handle WithProperties objects
+        words = properties.render(words)
+        if len(words) < 1:
+            return ["???"]
+        if len(words) == 1:
+            return ["'%s'" % words[0]]
+        if len(words) == 2:
+            return ["'%s" % words[0], "%s'" % words[1]]
+        return ["'%s" % words[0], "%s" % words[1], "...'"]
+
+    def createSummary(self, log):
+        Compile.createSummary(self, log)
+
+        if not self.warningPattern:
+            return
+
+        wre = self.warningPattern
+        if isinstance(wre, str):
+            import re
+            wre = re.compile(wre)
+
+        interested = True
+        if self.partialwarningStart:
+            interested = False
+        warnings = []
+        for line in log.getText().split("\n"):
+            if line == self.partialwarningStart:
+                interested = True
+            if line == self.partialwarningEnd:
+                interested = False
+            if interested and wre.match(line):
+                warnings.append(line)
+
+        if warnings:
+            self.addCompleteLog("partialwarnings", "\n".join(warnings) + "\n")
+
+        if self.partialwarningProperty is not None:
+            old_count = self.getProperty(self.partialwarningProperty, 0)
+            self.setProperty(self.partialwarningProperty, old_count + len(warnings), "PartialWarningsCompile")
+        else:
+            old_count = self.getProperty("partial-warnings-count", 0)
+            self.setProperty("partial-warnings-count", old_count + len(warnings), "PartialWarningsCompile")
