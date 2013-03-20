531fd6bea3cc2fa17b335df7df89f8245f10d721
Anders Waldenborg <anders@0x63.nu>
2013-03-20 12:15:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='buildbot-extras',
-    version='1',
+    version='2',
     url='https://github.com/wanders/buildbot-extras',
     author='Anders Waldenborg',
     author_email='anders@0x63.nu',
